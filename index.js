6db1eb3ca8e0516800c523d8b5b1230815359990
Parker <p.cronin.connolly@gmail.com>
2023-12-13 15:42:00 -0500

    need to put a lot more work on objects

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name: "Parker",
+    streetAddress: "12 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+ return  {
+        ...obj,
+        [key]:value,
+ }
+}
+
+const updatedKeyAndValue = updateEmployeeWithKeyAndValue(employee, "zip", "06897")
+console.log(updatedKeyAndValue)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+
+    return obj
+}
+const destructive = destructivelyUpdateEmployeeWithKeyAndValue(employee, "Macus", "somewhere")
+
+function  deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee}
+    delete newObj[key]
+    return newObj
+}
+
+console.log(deleteFromEmployeeByKey(employee, key))
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
+
+
+console.log(destructivelyDeleteFromEmployeeByKey(employee, 'name'))
